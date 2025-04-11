55c347716ae432d06c17495768b590bcc578d74f
Amalie0606 <amalielouiseh@hotmail.com>
2025-04-11 11:31:35 +0200

    Spring Boot notes

--- Lecture6_SpringBoot1/FirstProgram-HelloWorld/HeiController.java
+++ Lecture6_SpringBoot1/FirstProgram-HelloWorld/HeiController.java
@@ -10,3 +10,26 @@ public class HeiController {
         return "Hei verden "+navn;
     }
 }
+
+---------- MY NOTES -------------
+
+/* What is Spring Boot?
+- Spring Boot is a framework used to create stand-alone, production-grade Java-based applications.
+  It's part of the larger Spring Framework, which is a popular framework for building
+  enterprise-level applications in Java.
+
+-  Can be used with:
+- IDE's (Integrated Development Environments): IntelliJ IDEA, Eclipse, Visual Studio Code,  NetBeans.
+- Text Editors: Visual Studio Code, Sublime Text, Atom, Spring Tool Suite,  JBoss Developer Studio,
+  Android Studio, Cloud-Based IDEs, IntelliJ IDEA Community Edition.
+- Build Tools: Maven or Gradle.
+
+ // * What is Net Beans?
+
+// * What is a bean in Sprong Boot?
+
+ */
+ */
+
+
+
