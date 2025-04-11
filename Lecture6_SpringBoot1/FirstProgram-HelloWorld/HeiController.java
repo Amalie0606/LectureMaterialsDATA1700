c1881030f55e0abb0c783987fc30fd7d039b8de7
Amalie0606 <amalielouiseh@hotmail.com>
2025-04-11 11:42:19 +0200

    Spring Boot notes beans

--- Lecture6_SpringBoot1/FirstProgram-HelloWorld/HeiController.java
+++ Lecture6_SpringBoot1/FirstProgram-HelloWorld/HeiController.java
@@ -18,18 +18,36 @@ public class HeiController {
   It's part of the larger Spring Framework, which is a popular framework for building
   enterprise-level applications in Java.
 
--  Can be used with:
+- Can be used with:
 - IDE's (Integrated Development Environments): IntelliJ IDEA, Eclipse, Visual Studio Code,  NetBeans.
 - Text Editors: Visual Studio Code, Sublime Text, Atom, Spring Tool Suite,  JBoss Developer Studio,
   Android Studio, Cloud-Based IDEs, IntelliJ IDEA Community Edition.
 - Build Tools: Maven or Gradle.
 
- // * What is Net Beans?
+* What is Net Beans?
+- NetBeans is an open-source Integrated Development Environment (IDE) primarily used for Java development.
+  It supports various programming languages, but it is best known for its strong support for Java applications.
+  NetBeans provides a comprehensive set of tools to help developers create, debug, and manage applications,
+  with a focus on ease of use and robust functionality.
 
-// * What is a bean in Sprong Boot?
+* What is a bean in Sprong Boot?
+- A bean is simply a Java object that is instantiated, configured, and managed by the Spring container.
 
- */
- */
+- Defining a Bean in Spring Boot:
+- 1. Using @Component Annotation
+- 2. Using @Bean Annotation
+- 3. Using @Service, @Repository, and @Controller: More in depth:
+---> @Service: For service layer beans.
+---> @Repository: For data access beans (DAO layer).
+---> @Controller: For web controllers in Spring MVC.
+
+- Scopes/Types of Beans in Spring:
+- 1. Singleton Beans (Default)
+- 2. Prototype Beans
+- 3. Request, Session, and Global Session Scope (used in web applications)
+
+*/
+
 
 
 
