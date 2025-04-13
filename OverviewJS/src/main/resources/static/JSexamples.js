86d0dd1d1ecb4a409604f3ca610e1b660f6eca0a
Amalie0606 <amalielouiseh@hotmail.com>
2025-04-13 20:52:51 +0200

    The work is mysterious and important

--- OverviewJS/src/main/resources/static/JSexamples.js
+++ OverviewJS/src/main/resources/static/JSexamples.js
@@ -18,3 +18,18 @@ PLAY
 * The SQL murder mystery: https://mystery.knightlab.com/
  */
 
+/*
+References
+• https://www.slideshare.net/slideshow/data-structure-ppt-138483078/138483078#8
+• https://www.w3resource.com/data-structures-and-algorithms/array/array-data-
+structure.php
+• https://www.devx.com/terms/array-data-structure/
+• https://medium.com/nerd-for-tech/understanding-the-array-data-structure-3dba3f6d618b
+• https://www.geeksforgeeks.org/array-vs-arraylist-in-java/
+• https://www.geeksforgeeks.org/arraylist-in-java/
+• https://www.w3schools.com/java/java_arraylist.asp
+• https://stackoverflow.com/questions/2182597/time-complexity-for-java-arraylist
+• https://www.baeldung.com/java-collections-complexity
+• https://www.geeksforgeeks.org/introduction-to-tree-data-structure/
+ */
+
