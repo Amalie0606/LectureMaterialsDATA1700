31d414e2e443d160209074b28e3d463b7fb56805
Amalie0606 <amalielouiseh@hotmail.com>
2025-04-15 14:59:35 +0200

    start

--- Overview/x.js
+++ Overview/x.js
@@ -1,10 +1,9 @@
 
 
-/* --------------------------------------- OVERVIEW ----------------------------------------------
-                               Exam date: 22 May
+/* --------------------------------------- OVERVIEW ---------------------------------------------------
+                                       Exam date: 22 May
 
-Basic sources: https://www.w3schools.com/js/default.asp
-
+INTRODUCTION
 - JavaScript to program the behavior of web pages, in conjunction with HTML + CSS
 - Web programming language: can update and change HTML, CSS, calculate, manipulate and validate data.
 * Change HTML Content: JS HTML method getElementById().
@@ -16,9 +15,82 @@ Basic sources: https://www.w3schools.com/js/default.asp
 * Show HTML elements.
   Example: document.getElementById("demo").style.display = "block";
 
+WHERE TO
+OUTPUT
+STATEMENTS
+SYNTAX
+COMMENTS
+VARIABLES
+LET
+CONST
+OPERATORS
+ARITHMETIC
+ASSIGNMENT
+DATA TYPES
+FUNCTIONS
+OBJECTS
+OBJECT PROPERTIES
+OBJECT METHODS
+OBJECT DISPLAY
+OBJECT CONSTRUCTORS
+EVENTS
+STRINGS
+STRING METHODS
+STRING SEARCH
+STRING TEMPLATES
+NUMBERS
+BIGINT
+NUMBER METHODS
+NUMBER PROPERTIES
+ARRAYS
+ARRAY METHODS
+ARRAY SEARCH
+ARRAY SORT
+ARRAY ITERATION
+ARRAY CONST
+DATES
+DATE FORMATS
+DATE GET METHODS
+DATE SET METHODS
+MATH
+RANDOM
+BOOLEANS
+COMPARISONS
+IF ELSE
+SWITCH
+LOOP FOR
+LOOP FOR IN
+LOOP FOR OF
+LOOP WHILE
+BREAK
+ITERABLES
+SETS
+SET METHODS
+MAPS
+MAP METHODS
+TYPEOF
+TYPE CONVERSION
+DESTRUCTURING
+BITWISE
+REGEXP
+PRECEDENCE
+ERRORS
+SCOPE
+HOISTING
+STRICT MODE
+THIS KEYWORD
+ARROW FUNCTION
+CLASSES
+MODULES
+JSON
+DEBUGGING
+STYLE GUIDE
+BEST PRACTICES
+MISTAKES
+PERFORMANCE
+RESERVED WORDS
 
-
-
+Source: https://www.w3schools.com/js/default.asp
 
 LINKS
 * 20 Things JavaScript Developers Should know:
