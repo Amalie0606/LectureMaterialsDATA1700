23c58698cf4ee5b1ac033f1662efe8baf08b0036
Amalie0606 <amalielouiseh@hotmail.com>
2025-04-15 14:40:19 +0200

    .

--- Overview/x.js
+++ Overview/x.js
@@ -1,9 +1,26 @@
 
-/*
-Exam date: 22 May
+
+/* --------------------------------------- OVERVIEW ----------------------------------------------
+                               Exam date: 22 May
+
+Basic sources: https://www.w3schools.com/js/default.asp
+
+- JavaScript to program the behavior of web pages, in conjunction with HTML + CSS
+- Web programming language: can update and change HTML, CSS, calculate, manipulate and validate data.
+* Change HTML Content: JS HTML method getElementById().
+  Example: document.getElementById("demo").innerHTML = "Hello JavaScript";
+* Change style of an HTML element/CSS.
+  Example: document.getElementById("demo").style.fontSize = "35px";
+* Hide HTML elements.
+  Example: document.getElementById("demo").style.display = "none";
+* Show HTML elements.
+  Example: document.getElementById("demo").style.display = "block";
+
+
+
+
 
 LINKS
-
 * 20 Things JavaScript Developers Should know:
 https://www.youtube.com/playlist?list=PL1PqvM2UQiMoGNTaxFMSK2cih633lpFKP
 
@@ -11,7 +28,6 @@ https://www.youtube.com/playlist?list=PL1PqvM2UQiMoGNTaxFMSK2cih633lpFKP
 https://www.youtube.com/c/amigoscode?app=desktop
 
 PLAY
-
 * Code Combat: https://codecombat.com/play
 * Code Wars: https://www.codewars.com/
 * Screeps: https://screeps.com/
