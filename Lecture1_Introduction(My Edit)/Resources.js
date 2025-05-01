7e92dc855e616a1908ad9b1e9a8df7c668dfa38d
Amalie0606 <amalielouiseh@hotmail.com>
2025-05-01 19:40:06 +0200

    resourcez

---
+++ Lecture1_Introduction(My Edit)/Resources.js
@@ -0,0 +1,6 @@
+/*
+Resources: Lecture 1: Introduction:
+https://ruslanspivak.com/lsbaws-part1/
+https://www.w3schools.com/js/js_htmldom.asp
+https://www.w3schools.com/whatis/whatis_fullstack.asp
+ */
---
+++ Lecture2_JS1/Resources.js
@@ -0,0 +1,8 @@
+/*
+Resources. Lecture 2: Basic JavaScript
+• https://javascript.info/
+• https://www.w3schools.com/js/
+• https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference
+• https://ecma-international.org/publications-and-standards/standards/ecma-262/
+• https://www.linode.com/docs/guides/javascript-objects-tutorial/
+ */
