126422326696c35862736b10e7bcfe5735dde431
Amalie0606 <amalielouiseh@hotmail.com>
2025-05-08 22:34:00 +0200

    08.05

---
+++ Lecture11_JavaCollections/WeeklyTasks.js
@@ -0,0 +1,37 @@
+/*
+Exercises – Generic types
+1. Create a Person class that will implement a Comparable interface. Person class should
+implement compareTo method, that will verify if one person is taller than another.
+2. Create a simple Generic class that will give a possibility to store any kind of value
+within. Add object of type String, Integer and Double to array of that Generic type. Print
+all values of the array within a loop.
+
+Exercises – List
+1. Create a List and display its result (data should be provided by the user - console):
+a) Purchases to be made. *If an element already exists on the list, then it should
+not be added.
+b) *Add to the example above the possibility of "deleting" purchased elements
+c) Display only those purchases that start with „m” (e.g. milk)
+d) * View only purchases whose next product on the list starts with „m” (e.g. eggs,
+if milk was next on the list)
+2. Ratings received. Display their average. The numbers cannot be less than 1 and greater
+than 6.
+3. * List of lists - multiplication table
+
+Exercises – Set
+1. Create a set consisting of colors - given from the user.
+2. Present the removal of individual elements from the set.
+3. Display the collection before and after sorting.
+
+Exercises – Map
+1. Create a map and display its result (data should be provided by the user - console):
+a) Names and surnames
+b) Names and ages.
+c) Names and lists of friends (other names).
+d) * Names and details (map of maps), e.g.
+„Mike”:
+„ID”: „...”,
+„birthPlace” : „…”
+…
+
+ */
